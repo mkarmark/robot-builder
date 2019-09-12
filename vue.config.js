8170305537d07bff594922865ead6abaaef51b7c
Colby Tresness <cotresne@microsoft.com>
2019-09-12 12:09:30 -0700

    changed vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
+
+const path = require("path");
 module.exports = {
   configureWebpack: {
     devtool: "source-map"
+  },
+  chainWebpack: config => {
+    config
+      .entry("app")
+      .clear()
+      .add("./app/main.js")
+      .end();
+    config.resolve.alias
+      .set("@", path.join(__dirname, "./app"))
   }
 };
